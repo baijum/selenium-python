3ae4e4a4ee93f7366b24b82fec48b46d3127d6cb
Baiju Muthukadan <mbaiju@zeomega.com>
2014-03-19 18:18:33 +0530

    update year

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Selenium Python Bindings'
-copyright = u'2011-2013, Baiju Muthukadan'
+copyright = u'2011-2014, Baiju Muthukadan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -222,7 +222,7 @@ man_pages = [
 epub_title = u'Selenium Python Bindings'
 epub_author = u'Baiju Muthukadan'
 epub_publisher = u'Baiju Muthukadan'
-epub_copyright = u'2010-2011, Baiju Muthukadan'
+epub_copyright = u'2011-2014, Baiju Muthukadan'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
